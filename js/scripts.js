53c5bcdbf5c9933ac3e50f2c1ddad6fd0dfb6a45
Alan Hermanns and Christopher Burge <christopher@>
2019-08-26 16:32:47 -0700

    add css to body of page

--- js/scripts.js
+++ js/scripts.js
@@ -23,7 +23,7 @@ function Place(location, landmarks, activities){
 }
 
 var catalogOfDestinations = new PlacesDirectory;
-var tetons = new Place('The Tetons', 'Cathedral Group, Mount Owen, Tewwinot', 'Hiking, Skiing, Sightseeing');
+var tetons = new Place('The Tetons', 'Cathedral Group, Mount Owen, Teewinot', 'Hiking, Skiing, Sightseeing');
 var barbados = new Place('Barbados', 'Beautiful Beaches, Shallow Waters, Historic Architecture', 'Swimming, Snorkeling, Hiking, Leisure');
 var spain = new Place('Spain', 'Barcelona, Mallorca, la Sagrada Familia, Pyrenees', "Hiking, sightseeing, shopping");
 
@@ -37,25 +37,28 @@ $(document).ready(function() {
   $('#dest').change(function(event) {
     event.preventDefault();
     var selectValue = $('#dest').val()
-    $(".panel-info").show();
+    $(".panel-info").hide();
     $('img').hide();
     switch (parseInt(selectValue)) {
       case 1:
-        
+
       break;
       case 2:
+        $(".panel-info").show();
         $('h3').text(catalogOfDestinations.places[0].location);
         $('#item1').text(catalogOfDestinations.places[0].landmarks);
         $('#item2').text(catalogOfDestinations.places[0].activities);
         $('#tetons').show();
         break;
       case 3:
+        $(".panel-info").show();
         $('h3').text(catalogOfDestinations.places[1].location);
         $('#item1').text(catalogOfDestinations.places[1].landmarks);
         $('#item2').text(catalogOfDestinations.places[1].activities);
         $('#barbados').show();
         break;
       case 4:
+        $(".panel-info").show();
         $('h3').text(catalogOfDestinations.places[2].location);
         $('#item1').text(catalogOfDestinations.places[2].landmarks);
         $('#item2').text(catalogOfDestinations.places[2].activities);
