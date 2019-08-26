daa9091b51cd8dd54239b928d59c9a056c90c755
Alan Hermanns and Christopher Burge <christopher@>
2019-08-26 10:17:53 -0700

    add structure to html and start the place object.

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+function PlacesDirectory(){
+  this.places = [];
+}
+
+function Place(location, landmarks, timeOfYear, activities){
+  this.location = location;
+  this.landmarks = landmarks;
+  this.timeOfYear = timeOfYear;
+  this.activities = activities;
+}
